c78f48c9fdf6f6a02860dfaff0c854b63cd062c7
Zachary <zacarlin@gmail.com>
2019-09-15 16:35:20 -0400

    cleaned up project structure, added text to about page, removed unused helloworld component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>Analyaze text in pictures with your browser</p>
+    <p>Analyaze text from pictures with your browser made with Vue.js and Tesseract.js (A pure Javascript port of the popular Tesseract OCR engine).</p>
   </div>
 </template>
